bfec7238bf3b0a45f31e6f2c778069d11199fab2
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-11 20:32:46 +0900

    #2 유저 클래스 수정

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,8 +3,9 @@ package model;
 import java.util.Random;
 
 public class User {
-
+    private String username;
     private int HP;
+    boolean is_saved;
 
     public int getPower() {
         Random rand = new Random();
@@ -21,5 +22,27 @@ public class User {
 
     public User(int HP) {
         this.HP = HP;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isIs_saved() {
+        return is_saved;
+    }
+
+    public void setIs_saved(boolean is_saved) {
+        this.is_saved = is_saved;
+    }
+
+    public User(String username, int HP, boolean is_saved) {
+        this.username = username;
+        this.HP = HP;
+        this.is_saved = is_saved;
     }
 }
